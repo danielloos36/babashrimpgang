1f1ee239d6c3647990e69cc19b06ce0f068a5b9c
danielloos36 <danielloos36@gmail.com>
2022-10-15 01:32:27 +0200

    gsap eingebunden

--- index.js
+++ index.js
@@ -29,3 +29,53 @@ function burgerIcons() {
     document.getElementsByClassName("bar2")[0].classList.remove("change");
     document.getElementsByClassName("bar3")[0].classList.remove("change");
 }
+
+gsap.registerPlugin(ScrollTrigger);
+
+tl = gsap.timeline({
+  scrollTrigger: { 
+    trigger: '.ueber-uns',
+    toggleActions: 'play none none reverse',
+    start: 'top bottom'
+  },
+  repeat: -1
+  },);
+  tl.to(".lampe", {
+    rotation: -4,
+    duration: 1.5,
+    transformOrigin: "top center",
+    ease:"power3"
+  },);
+  tl.to(".lampe", {
+    rotation: 4,
+    duration: 1.5,
+    transformOrigin: "top center",
+    ease:"power3"
+  },);
+
+  tl = gsap.timeline({
+    scrollTrigger: { 
+      trigger: '.kontaktformular',
+      toggleActions: 'play',
+    },
+    repeat: -1
+    },);
+    tl.to(".kk", {
+      rotation: 360,
+      duration: 25,
+      transformOrigin: "center",
+      ease: "none"
+    },);
+    tl = gsap.timeline({
+      scrollTrigger: { 
+        trigger: '.kontaktformular',
+        toggleActions: 'play',
+      },
+      repeat: -1
+      },);
+      tl.to(".vwl", {
+        rotation: -360,
+        duration: 22,
+        transformOrigin: "center",
+        ease: "none"
+      },);
