2424b944189736b2a3155a980a65bdad2b1833a5
danielloos36 <danielloos36@gmail.com>
2023-01-08 15:26:40 +0100

    update

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 function myFunction() {
 var x = document.getElementById("navbar-collapsed");
+<<<<<<< Updated upstream
 if (x.style.top === "0px") {
   x.style.top = "3000px";
+=======
+if (x.style.left === "-10vw") {
+  x.style.left = "100vw";
+>>>>>>> Stashed changes
   document.getElementsByClassName("icons")[0].style.fill = "black";
   document.getElementsByClassName("icons")[1].style.fill = "black";
   document.getElementsByClassName("icons")[2].style.fill = "black";
 } else {
+<<<<<<< Updated upstream
   x.style.top = "0px";
+=======
+  x.style.left = "-10vw";
+>>>>>>> Stashed changes
   document.getElementsByClassName("icons")[0].style.fill = "white";
   document.getElementsByClassName("icons")[1].style.fill = "white";
   document.getElementsByClassName("icons")[2].style.fill = "white";
@@ -14,7 +23,7 @@ if (x.style.top === "0px") {
 }
 function linkClicked() {
     var x = document.getElementById("navbar-collapsed");
-    x.style.top = "3000px";
+    x.style.left = "100vw";
     document.getElementsByClassName("icons")[0].style.fill = "black";
     document.getElementsByClassName("icons")[1].style.fill = "black";
     document.getElementsByClassName("icons")[2].style.fill = "black";
